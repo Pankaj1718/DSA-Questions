91f3b1bd50cd66f653cd995e7a5454548a461952
Pankaj1718 <143487381+Pankaj1718@users.noreply.github.com>
2024-01-24 01:14:39 +0530

    arr_object_pass

--- rough3.cpp
+++ rough3.cpp
@@ -1,29 +1,32 @@
-/*#include<iostream>
-using namespace std;
-int main()
-{  
-    
-     int *arr = new int[5];
-    arr[0] = 33;
-    arr[1] = 56;
-    arr[2] = 23;
-    arr[3] = 56;
-    arr[4] = 78;
-    cout<<"the value of arr[0] is "<<arr[0]<<endl;
-    cout<<"the value of arr[1] is "<<arr[1]<<endl;
-    cout<<"the value of arr[2] is "<<arr[2]<<endl;
-    cout<<"the value of arr[3] is "<<arr[3]<<endl;
-    cout<<"the value of arr[4] is "<<arr[4]<<endl;
-    return 0;
-}
-*/
 #include<iostream>
 using namespace std;
+
+class complex{
+      int a;
+      int b;
+    public:
+      void Setdata(int a1,int b1){
+        a = a1;
+        b = b1;
+      }
+      void SetdataBySum(complex o1,complex o2){
+        a = o1.a + o2.a;
+        b = o1.b + o2.b;
+      }
+      void Printdata(void){
+         cout<<"Your complex number is "<<a<<"+"<<b<<"i"<<endl;
+      }
+};
 int main()
 {
-    int *a = new int (1);
-   // delete a;
-    cout<<" the address of a is "<<a;
-    cout<<" the value at a is "<<*a;
+    complex c1,c2,c3;
+    c1.Setdata(1,3);
+    c1.Printdata();
+
+    c2.Setdata(2,4);
+    c2.Printdata();
+
+    c3.SetdataBySum(c1,c2);
+    c3.Printdata();
     return 0;
 }
