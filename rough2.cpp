c4f92317f2e85fb73625204b88eb8f5622227c36
Pankaj1718 <143487381+Pankaj1718@users.noreply.github.com>
2024-01-24 01:10:06 +0530

    virtual function

--- rough2.cpp
+++ rough2.cpp
@@ -1,2 +1,27 @@
-Panchar putram
-Kabhi na mitram
+#include<iostream>
+using namespace std;
+
+class  base{
+      public:
+        
+        virtual void display(){
+            cout<<"base display called "<<endl;
+        }
+};
+
+class derived : public base{
+    public: 
+    void display(){
+        cout<<"derived display called "<<endl;
+    }
+};
+int main()
+{  
+   derived d1;
+   base* bptr= &d1;
+   bptr->display();
+
+   base b1;
+   b1.display();
+    return 0;
+}
