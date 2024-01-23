04374fcd26ae7994eeb10e011cbc5fe2c9bb976b
Pankaj1718 <143487381+Pankaj1718@users.noreply.github.com>
2024-01-24 01:12:48 +0530

    xyz

--- rough.cpp
+++ rough.cpp
@@ -1,11 +1,25 @@
 #include<iostream>
 using namespace std;
-#include <math.h>
+
+template <class t1,class t2>
+class vector1{
+    public :
+      t1 real1;
+      t2 complex1;
+      vector1(t1 a, t2 b){
+          real1 = a;
+          complex1 = b;
+      }
+
+      virtual void display(){
+        cout<<"The value of vector1 is "<<real1
+            <<" + "<<complex1<<"i"<<endl;
+      }
+};
+
 int main()
 {
-    int angle;
-    cout<<"enter an angle"<<endl;
-    cin>>angle;
-    cout<<"The cos angle of angle "<<angle<<" is "<<cos(int(angle));
+    vector1<float,float>v(6.7,5.4);
+    v.display();
     return 0;
 }
